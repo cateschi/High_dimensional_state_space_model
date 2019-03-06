0ed074275c6b33f02f1ceba525bff89bc1aa1534
Caterina Schiavoni <caterina.schiavoni@hotmail.it>
2019-03-06 10:17:04 +0100

    Update LFS_model_with_GT_lags.R

--- LFS_model_with_GT_lags.R
+++ LFS_model_with_GT_lags.R
@@ -14,7 +14,7 @@
 # ns.id: vector of length equal to the number of nonstationary idiosyncratic components. It should contain the ordered numbers corresponing to the nonstationary idiosyncratic components.
 # psis: vector with the estimated coefficients from the OLS regression of the estimated LFS trend's slope's first difference on the lags of the differenced Google Trends' factor.
 
-# The lines that are not commented here, are commented on KalmanFilter/LFS_model.R and KalmanFilter/LFS_model_with_GT.R. 
+# The lines that are not commented here, are commented on /LFS_model.R and /LFS_model_with_GT.R. 
 
 # Packages required to run the scripts:
 library(magic)
